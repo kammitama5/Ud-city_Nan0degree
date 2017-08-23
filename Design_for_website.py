01233147933ec44f3d4757ea9f465089fa4c64a0
kammitama5 <krystal.maughan@gmail.com>
2017-08-22 20:48:34 -0700

    notes and plan for Final Project using instructor notes

---
+++ Design_for_website.py
@@ -0,0 +1,23 @@
+'''
+-both movie and Tvshow would inherit title and duration from Video
+
+class Movie(Video):
+-title
+-storyline
+-poster_image
+-youtube_trailer
+
+def show_trailer()
+
+class TvShow(Video):
+-title
+-season
+-episode
+-tv_station
+
+def get_local_listing()
+
+class Video():
+-title
+-duration
+'''
