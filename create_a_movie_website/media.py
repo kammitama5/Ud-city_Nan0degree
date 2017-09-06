da9073e34e1b7722164a3b2721e5f78b9dd5fa5c
kammitama5 <krystal.maughan@gmail.com>
2017-09-06 14:46:59 -0700

    update comments

--- create_a_movie_website/media.py
+++ create_a_movie_website/media.py
@@ -1,73 +1,21 @@
 #!python2
-'''
-Things for class movie to remember:
-- title 
-- storyline
-- poster_image
-- trailer_youtube
-'''
-'''
--both movie and Tvshow would inherit title and duration from Video
-
-class Movie(Video):
--title
--storyline
--poster_image
--youtube_trailer
-
-def show_trailer()
-
-class TvShow(Video):
--title
--season
--episode
--tv_station
-
-def get_local_listing()
-
-class Video():
--title
--duration
-'''
+## import webrowser module
 import webbrowser
+
+## initialize class
 class Movie():
+	
+	## define constructor method with arguments -> movie_title, movie_storyline, poster_image, trailer_youtube
+	## instances of method defined
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
+		
+	## open trailer from Youtube url using webbrowser
 	
 	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+		webbrowser.open(self.trailer_youtube_url) ## instance of method
 	
 	
-'''
-Things for class movie to remember:
-- title 
-- storyline
-- poster_image
-- trailer_youtube
-'''
-'''
--both movie and Tvshow would inherit title and duration from Video
-
-class Movie(Video):
--title
--storyline
--poster_image
--youtube_trailer
-
-def show_trailer()
-
-class TvShow(Video):
--title
--season
--episode
--tv_station
-
-def get_local_listing()
-
-class Video():
--title
--duration
-'''
